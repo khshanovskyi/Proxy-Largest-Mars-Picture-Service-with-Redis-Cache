2be23331968ce4c71b574d39f28350604b955b25
khshanovskyi <khshanovskyi@gmail.com>
2022-10-19 11:58:11 +0300

    Initial commit

---
+++ src/test/java/ua/khshanovskyi/ProxyLargestMarsPictureServiceWithRedisCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.khshanovskyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyLargestMarsPictureServiceWithRedisCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
